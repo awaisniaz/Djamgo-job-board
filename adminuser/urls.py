0b5f3d7ffb82ca530c2645ceddb8431996734314
awais.niaz@mwanmobile.org <awais.niaz@mwanmobile.org>
2023-07-12 21:44:20 +0500

    Sign and login design implementations

--- adminuser/urls.py
+++ adminuser/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from adminuser import views
 urlpatterns = [
-
+    path('login/', views.login, name="login"),
+    path("signup/",views.register,name="signup"),
+    path("home/",views.home,name="home")
 ]
